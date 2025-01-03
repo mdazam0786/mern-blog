000cf0ae0c30e69af00f44824486ea171747765c
MD NAIYER AJAM <mdnaiyerajam@MDs-MacBook-Air.local>
2025-01-03 22:54:28 +0530

    create-pages-and-routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path='/Sign-in' element={<Signin/>}/>
+        <Route path="/Sign-up" element={<Signup/>}/>
+        <Route path='/Dashboard' element={<Dashboard/>}/>
+        <Route path="/Projects" element={<Projects/>}/>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
